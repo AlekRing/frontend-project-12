104ab68face7392346a2df5b22723f228aced042
alekRing <okolcov60@gmail.com>
2023-03-03 18:17:16 +0200

    chat: fix

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Navbar from '../components/Navbar';
-import PrivateRoute from '../containers/PrivateRoute';
+import PrivateRoute from '../containers/privateRoute';
 import ChatPage from '../pages/ChatPage';
 import LoginPage from '../pages/LoginPage';
 import NotFound from '../pages/NotFound';
